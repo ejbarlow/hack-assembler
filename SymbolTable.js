3214a8745d58c262dd03f3b506c78d570408b635
EJ Barlow <ed@ejbarlow.co.uk>
2022-07-06 02:08:21 +0100

    Symbol table and start script.

---
+++ SymbolTable.js
@@ -0,0 +1,33 @@
+class SymbolTable {
+  constructor() {
+    this.symbols = [];
+    this.next = 16;
+  }
+
+  addSymbol = (symbol) => {
+    symbols.push(symbol);
+  }
+
+  addSymbols = (symbols) => {
+    symbols.push(...symbols);
+    console.log(symbols);
+  }
+
+  getAddress = (symbol) => {
+    const existingSymbol = symbols.find(sym => sym.symbol === symbol);
+    
+    if (existingSymbol) {
+      return existingSymbol.address;
+    };
+
+    symbols.push({
+      symbol,
+      address: next,
+    });
+
+    next += 1;
+    return symbols[symbols.length - 1].address;
+  }
+}
+
+export { SymbolTable }
--- index.js
+++ index.js
@@ -1,8 +1,37 @@
+import { SymbolTable } from "./SymbolTable.js";
+
 /*****************
  * Pre iteration
  *****************/
 // Initialise a symbol table
+const sym = new SymbolTable();
 // Populate with predefined symbols
+sym.addSymbols([
+  { symbol: 'R0', address: 0 },
+  { symbol: 'R1', address: 1 },
+  { symbol: 'R2', address: 2 },
+  { symbol: 'R3', address: 3 },
+  { symbol: 'R4', address: 4 },
+  { symbol: 'R5', address: 5 },
+  { symbol: 'R6', address: 6 },
+  { symbol: 'R7', address: 7 },
+  { symbol: 'R8', address: 8 },
+  { symbol: 'R9', address: 9 },
+  { symbol: 'R10', address: 10 },
+  { symbol: 'R11', address: 11 },
+  { symbol: 'R12', address: 12 },
+  { symbol: 'R13', address: 13 },
+  { symbol: 'R14', address: 14 },
+  { symbol: 'R15', address: 15 },
+  { symbol: 'SP', address: 0 },
+  { symbol: 'LCL', address: 1 },
+  { symbol: 'ARG', address: 2 },
+  { symbol: 'THIS', address: 3 },
+  { symbol: 'THAT', address: 4 },
+  { symbol: 'SCREEN', address: 16384 },
+  { symbol: 'KBD', address: 24576 },
+]);
+
 /**************
  * First pass
  **************/
